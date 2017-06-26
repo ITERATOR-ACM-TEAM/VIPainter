41180ac99ed8369f4b7f9e8082864c7c1e606f3d
Penn000 <Penn000@nwsuaf.edu.cn>
2017-06-26 18:04:34 +0800

    123

--- vpoint.cpp
+++ vpoint.cpp
@@ -3,3 +3,8 @@
 VPoint::VPoint():x(0), y(0){}
 
 VPoint::VPoint(double x,double y):x(x), y(y){}
+
+const VPoint& VPoint::operator=(const VPoint &point){
+    VPoint p(point);
+    return p;
+}
--- vpoint.h
+++ vpoint.h
@@ -1,12 +1,13 @@
 #ifndef VPOINT_H
 #define VPOINT_H
 
-class VPoint{
+class VPoint{   
 public:
     double x;
     double y;
     VPoint();
     VPoint(double x,double y);
+    const VPoint& operator=(const VPoint &point);
 };
 
 #endif //#ifndef VPOINT_H
