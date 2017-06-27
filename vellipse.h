6a24e9338f483d5620aa53d03caa60cb62444d89
kkke <kkke@nwsuaf.edu.cn>
2017-06-27 09:48:12 +0800

    update vshape add size()

--- vellipse.h
+++ vellipse.h
@@ -2,15 +2,23 @@
 #define VELLIPSE_H
 
 #include "vshape.h"
+#include "vpoint.h"
 
 class VEllipse : public VShape{
 private:
+    VPoint center;
+    double alpha;
 
 public:
     VEllipse();
     VEllipse(const VEllipse &shape);
     ~VEllipse();
     const VEllipse& operator=(const VEllipse &shape);
+
+    QImage toImage()override;
+    void resize(int weight,int height)override;
+    void rotate(const VPoint &center,double alpha)override;
+    ~VEllipse()override;
 };
 
 #endif // VELLIPSE_H
--- vshape.h
+++ vshape.h
@@ -7,6 +7,7 @@
 class VShape{
 public:
     virtual QImage toImage()=0;
+    virtual VPoint size()=0;
     virtual void resize(int weight,int height)=0;
     virtual void rotate(const VPoint &center,double alpha)=0;
     virtual ~VShape();
