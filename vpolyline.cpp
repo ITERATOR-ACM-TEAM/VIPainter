04ab283cf37708122bb04259d52a37b402f9b323
Penn000 <Penn000@nwsuaf.edu.cn>
2017-06-27 09:49:46 +0800

    modified volyline

--- vpolyline.cpp
+++ vpolyline.cpp
@@ -1,5 +1,36 @@
 #include "vpolyline.h"
 
-VPolyline::VPolyline()
+VPolyline::VPolyline():n(0)
 {
+}
+
+VPolyline::VPolyline(const VPolyline &polyline){
+    this->n = polyline.getN();
+    vertex.clear();
+    for(auto &i: polyline.getPointList()){
+        this->vertex.append(i);
+    }
+}
+
+int VPolyline::getN()const{
+    return n;
+}
+
+void VPolyline::move(int i,VPoint p){//move the ith point to position p(x,y)
+    if(i >= n-1)i = n-1;
+    if(i < 0)i = 0;
+    this->vertex[i].x = p.x;
+    this->vertex[i].y = p.y;
+}
+
+QList<VPoint> VPolyline::getPointList()const{
+    return this->vertex;
+}
+
+void VPolyline::addPoint(VPoint p){
+    this->vertex.push_back(p);
+}
+
+const VPolyline& VPolyline::operator=(const VPolyline &polyline){
+    QVector<VPoint> vec = polyline.getPointList();
 }
--- vpolyline.h
+++ vpolyline.h
@@ -2,16 +2,21 @@
 #define VPOLYLINE_H
 
 #include "vshape.h"
-#include "QList"
+#include "QVector"
 #include "vpoint.h"
 
 class VPolyline:public VShape
 {
+private:
+    int n;//折线端点数目
+    QVector<VPoint> vertex;//折线的各个端点
 public:
     VPolyline();
     VPolyline(const VPolyline &polyline);
-    virtual move(int i,int x,int y);//move the ith point to (x,y)
-    virtual QList<Vpoint> getPointList();
+    int getN() const;
+    virtual void addPoint(VPoint p);
+    virtual void move(int i,VPoint p);//move the ith point to position p(x,y)
+    virtual QVector<VPoint> getPointList() const;
     const VPolyline& operator=(const VPolyline &polyline);
 };
 
