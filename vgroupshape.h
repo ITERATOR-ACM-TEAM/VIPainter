15d6e4fbfaea8160b5d6239c7e0ad8ad9a3adf98
kkke <kkke@nwsuaf.edu.cn>
2017-06-27 15:42:30 +0800

    update

--- vgroupshape.h
+++ vgroupshape.h
@@ -10,17 +10,18 @@
 class VGroupShape : public VShape
 {
 private:
-    QVector<QPair<VShape*>> ShapeVector;
+    QVector<VShape*> ShapeVector;
 public:
     VGroupShape();
     VGroupShape(const VGroupShape &shape);
     static VGroupShape* fromJsonObject(const QJsonObject &jsonObject);
     ~VGroupShape()override;
     const VGroupShape& operator=(const VGroupShape &shape);
-    void addShape(VShape * other, const VPoint & location);
+    //void addShape(VShape * other, const VPoint & location);
     bool eraseShape(int i, const VPoint & location);
-    bool moveShape(int i, const VPoint & location);
+    //bool moveShape(int i, const VPoint & location);
     QVector<VShape *> getShapeVector();
 };
 
 #endif // VGROUPSHAPE_H
+
