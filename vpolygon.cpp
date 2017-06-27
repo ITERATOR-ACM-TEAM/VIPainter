fa1e6ddbd57a844cf3686b257975793e013931e4
Penn000 <Penn000@nwsuaf.edu.cn>
2017-06-27 15:14:58 +0800

    modified

--- vpolygon.cpp
+++ vpolygon.cpp
@@ -2,22 +2,23 @@
 #include "vpoint.h"
 #include "QDebug"
 
-VPolygon::VPolygon(){
+VPolygon::VPolygon():VPolyline(){
 }
 
 VPolygon::~VPolygon(){
 }
 
-VPolygon::VPolygon(const VPolygon &shape){
+VPolygon::VPolygon(const VPolygon &shape):VPolyline(){
     n = shape.getN();
     vertex.clear();
     for(auto &i : shape.vertex){
         this->vertex.append(i);
     }
+    this->getCircumscribedRectangle();
 }
 
-QImage toImage(){
-    int width = cr2.y-cr1.y, height = cr2.x-cr1.x;
+QImage VShape::toImage(){
+    int width = VPolyline::cr2.y-VPolyline::cr1.y, height = VPolyline::cr2.x-VPolyline::cr1.x;
     QImage image(width, height, QImage::Format_ARGB32);
     QPainter painter(&image);
     QPolygonF qpf;
