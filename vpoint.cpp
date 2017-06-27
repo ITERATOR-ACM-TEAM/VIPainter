23d50540c759c9fb5d192ea64a9168c0d3f760f5
Penn000 <Penn000@nwsuaf.edu.cn>
2017-06-27 08:29:21 +0800

    modified opearter=

--- vpoint.cpp
+++ vpoint.cpp
@@ -5,6 +5,7 @@ VPoint::VPoint():x(0), y(0){}
 VPoint::VPoint(double x,double y):x(x), y(y){}
 
 const VPoint& VPoint::operator=(const VPoint &point){
-    VPoint p(point);
-    return p;
+    this->x = point.x;
+    this->y = point.y;
+    return *this;
 }
