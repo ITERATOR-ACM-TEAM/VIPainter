eb4a9bcd79f51cdf68deb48bc7765b5e1f658a5d
kkke <kkke@nwsuaf.edu.cn>
2017-06-30 15:40:50 +0800

    update

--- vgroupshape.cpp
+++ vgroupshape.cpp
@@ -252,7 +252,7 @@ bool VGroupShape::contains(VPoint point)
         subAngle = it->getAngle();
         subMag = it->getMagnification();
         subPoint = VPoint(point.x - subLocation.x, point.y - subLocation.y).rotate(VPoint(0,0),-subAngle)*subMag;
-        if(it->contains(point))
+        if(it->contains(subPoint))
         {
             return true;
         }
