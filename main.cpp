3759b48a842eab2593cabff462f1cdbe2fb688a8
kkke <kkke@nwsuaf.edu.cn>
2017-07-02 16:01:03 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QDesktopWidget>
+#include <QDir>
+#include <QDebug>
+#include <QString>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+#if defined(Q_OS_WIN32)
+#elif defined(Q_OS_LINUX)
+    QDir home(QDir::home());
+    QString workdir=".VIPainter";
+    if(!QDir(home.filePath(workdir)).exists())home.mkdir(workdir);
+//    qDebug()<<home.filePath(workdir);
+    QDir::setCurrent(home.filePath(workdir));
+//    qDebug()<<QDir::currentPath();
+#endif
+
     MainWindow w;
     w.show();
 
+#if defined(Q_OS_WIN32)
+    QDesktopWidget *deskdop = QApplication::desktop();
+    move((deskdop->width() - this->width())/2, (deskdop->height() - this->height())/2);
+#elif defined(Q_OS_LINUX)
     QDesktopWidget *dw = QApplication::desktop();
     QRect parentRect = dw->availableGeometry(dw->primaryScreen());
 
@@ -16,6 +34,7 @@ int main(int argc, char *argv[])
     }
 
     w.move(parentRect.center() - w.geometry().center());
+#endif
 
     return a.exec();
 }
